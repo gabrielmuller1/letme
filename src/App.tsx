ce20c56008b6d4c80d6342e43d576ad0baadd492
Gabriel Muller <gabriel.m@pormade_domain.local>
2022-03-03 18:23:30 -0300

    Primeiro upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Route path="/" exact component={Home}/>
+    <Route path="/rooms/news" component={NewRoom}/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
