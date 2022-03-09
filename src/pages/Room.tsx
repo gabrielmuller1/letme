f23dbb602875be8c6d95ec5d4ec2cf9a39f5bdab
Gabriel Muller <gabriel.m@pormade_domain.local>
2022-03-09 08:43:31 -0300

    Estrutura das perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -4,13 +4,14 @@ import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from "../components/Button";
+import { Question } from "../components/Question";
 import { RoomCode } from "../components/RoomCode";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 
 import "../styles/room.scss";
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -42,7 +43,7 @@ export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState("");
 
   const roomId = params.id;
@@ -136,7 +137,17 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
